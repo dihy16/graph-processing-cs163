2c8043bb7d4c067a855ff447e3322e61425634c8
dinhhogiahuy290605@gmail.com <dinhhogiahuy290605@gmail.com>
2024-06-27 22:27:49 +0700

    created RouteVarQuery.py

---
+++ 23125036_src/RouteVarQuery.py
@@ -0,0 +1,10 @@
+class RouteVarQuery:
+    def __init__(self):
+        self.route_var_list = []
+
+    def searchByABC():
+        raise NotImplementedError
+    def outputAsCSV():
+        raise NotImplementedError
+    def outputAsJSON():
+        raise NotImplementedError
