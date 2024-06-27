dfbee4d0deac815bfedc10c04b0855b490171c6b
dinhhogiahuy290605@gmail.com <dinhhogiahuy290605@gmail.com>
2024-06-27 22:34:45 +0700

    added search by properties functions

--- 23125036_src/RouteVarQuery.py
+++ 23125036_src/RouteVarQuery.py
@@ -1,9 +1,38 @@
-class RouteVarQuery:
-    def __init__(self):
-        self.route_var_list = []
+from RouteVar import *
 
-    def searchByABC():
-        raise NotImplementedError
+
+class RouteVarQuery:
+    def __init__(self, route_var_list):
+        self.RouteVar_list = route_var_list
+
+    def searchByRouteID(self, routeID):
+        lst = [route_var for route_var in self.RouteVar_list if route_var.getRouteID() == routeID]
+        return lst
+    def searchByRouteVarID(self, routeVarID):
+        lst = [route_var for route_var in self.RouteVar_list if route_var.getRouteVarID() == routeVarID]
+        return lst
+    def searchByRouteVarName(self, routeVarName):
+        lst = [route_var for route_var in self.RouteVar_list if route_var.getRouteVarName() == routeVarName]
+        return lst
+    def searchByRouteShortName(self, routeVarShortName):
+        lst = [route_var for route_var in self.RouteVar_list if route_var.getRouteShortName() == routeVarShortName]
+        return lst
+    def searchByStartStop(self, startStop):
+        lst = [route_var for route_var in self.RouteVar_list if route_var.getStartStop() == startStop]
+        return lst
+    def searchByEndStop(self, endStop):
+        lst = [route_var for route_var in self.RouteVar_list if route_var.getEndStop() == endStop]
+        return lst
+    def searchByDistance(self, distance):
+        lst = [route_var for route_var in self.RouteVar_list if route_var.getDistance() == distance]
+        return lst
+    def searchByOutbound(self, outbound):
+        lst = [route_var for route_var in self.RouteVar_list if route_var.getOutbound() == outbound]
+        return lst
+    def searchByRunningTime(self, runningTime):
+        lst = [route_var for route_var in self.RouteVar_list if route_var.getRunningTime() == runningTime]
+        return lst
+
     def outputAsCSV():
         raise NotImplementedError
     def outputAsJSON():
