cc5e92378a963314adf8a2743bc8a4809d6262f3
dinhhogiahuy290605@gmail.com <dinhhogiahuy290605@gmail.com>
2024-06-29 16:35:42 +0700

    testing for loadData functions

--- 23125036_src/_23125036_src.py
+++ 23125036_src/_23125036_src.py
@@ -1,1 +1,7 @@
+﻿from RouteVarQuery import RouteVarQuery
+
+route_var_query = RouteVarQuery([])
+route_var_query.inputFromJSON('data/vars.json')
+route_var_query.outputAsJSON(route_var_query.searchByRouteId(35), 'data/route_var_output.json')
+route_var_query.outputAsCSV(route_var_query.searchByStartStop('Bến xe buýt Kho Muối'), 'data/route_var_output.csv')
 
