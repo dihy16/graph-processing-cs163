add52c0e274879248c507da8bca0e9aec663be4f
dinhhogiahuy290605@gmail.com <dinhhogiahuy290605@gmail.com>
2024-07-08 23:53:27 +0700

    created file to convert coordinates from (lat,lng) to (x,y)

--- 23125036_src/LLtoXY.py
+++ 23125036_src/LLtoXY.py
@@ -5,8 +5,8 @@ crs_3405 = CRS.from_epsg(3405)
 
 transformer = Transformer.from_crs(crs_wgs84, crs_3405, always_xy=True)
 
-def convertLngLatToXY(Lng, Lat):
-    x, y = transformer.transform(Lng, Lat)
+def convertLngLatToXY(Lat, Lng):
+    x, y = transformer.transform(Lat, Lng)
     return x, y
 
 
