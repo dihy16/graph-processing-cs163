bb35a382a51cdca9a7c4253173163a720835db92
dinhhogiahuy290605@gmail.com <dinhhogiahuy290605@gmail.com>
2024-08-07 21:44:18 +0700

    saved main file

--- 23125036_src/_23125036_src.py
+++ 23125036_src/_23125036_src.py
@@ -39,7 +39,7 @@ Build edges from scratch
 # graph.build_graph(route_var_query, stop_query, path_query, stop_indices_dict)
 # print(time.time() - start)
 
-# """
+# """0.
 # Save edges to file
 # """
 # graph.output_edges_as_JSON('data/GraphEdges.json')
@@ -48,7 +48,12 @@ Build edges from scratch
 Input edges from file
 """
 graph.input_edges_from_JSON('data/GraphEdges.json')
-# graph.export_path_2_stops(0, 1)
+start, stop = random.sample(uniqueIds, 2)
+start = stop_indices_dict[start]
+stop = stop_indices_dict[stop]
+path, coors = graph.export_path_2_stops(start, stop, uniqueIds, stop_list, 'data/coordinates.geojson')
+print(path)
+print(coors)
 # """
 # Run dijkstra for all pairs, then export to file
 # """
@@ -68,49 +73,49 @@ graph.input_edges_from_JSON('data/GraphEdges.json')
 
 """
 Comparing run time between Dijkstra and Bidirectional Dijkstra on CH 
-"""
-start = 0
-stop = graph.num_nodes - 1
-num_pairs = 10000
-pairs = generate_multiple_pairs(start, stop, num_pairs)
+# """
+# start = 0
+# stop = graph.num_nodes - 1
+# num_pairs = 10000
+# pairs = generate_multiple_pairs(start, stop, num_pairs)
 
-dijk_time = 0
-dijk_dist = []
+# dijk_time = 0
+# dijk_dist = []
 
-print('Querying with Dijkstra...')
-start = time.time() 
-for pair in pairs:
-    source, target = pair
-    ans = graph.dijkstra_one_pair(source, target)
-    dijk_dist.append((source, target, ans))
-dijk_time = time.time() - start
-print(dijk_time)
+# print('Querying with Dijkstra...')
+# start = time.time() 
+# for pair in pairs:
+#     source, target = pair
+#     ans, _, _ = graph.dijkstra_one_pair_with_trace(source, target)
+#     dijk_dist.append((source, target, ans))
+# dijk_time = time.time() - start
+# print(dijk_time)
 
-CH = ContractionHierarchies(graph)
-start = time.time()
-print('Computing CH...')
-CH.preprocess_graph()
-CHtime = time.time() - start
-bidijk_dist = []
+# CH = ContractionHierarchies(graph)
+# start = time.time()
+# print('Computing CH...')
+# CH.preprocess_graph()
+# CHtime = time.time() - start
+# bidijk_dist = []
 
-start = time.time() 
+# start = time.time() 
 
-print('Querying with CH...')
-for pair in pairs:
-    source, target = pair 
-    bidijk_dist.append(graph.bidirectional_dijkstra(source, target))
-bidijkTime = time.time() - start
+# print('Querying with CH...')
+# for pair in pairs:
+#     source, target = pair 
+#     bidijk_dist.append(graph.bidirectional_dijkstra(source, target))
+# bidijkTime = time.time() - start
 
-print(bidijkTime, 'secs')
-print('RESULT:')
-print('Vertex count: ', graph.num_nodes)
-print('Edge count: ', graph.num_edges)
-print('Dijkstra time: ', dijk_time)
-print('CH compute time: ', CHtime)
-print('CH query time: ', bidijkTime)
-cnt = 0
-for i in range(0, len(dijk_dist)):
-    if abs(dijk_dist[i][2] - bidijk_dist[i]) > PRECISION_TOLERANCE:
-        cnt += 1
-        print(dijk_dist[i][0], dijk_dist[i][1], dijk_dist[i][2], bidijk_dist[i])
-print('{} / {}'.format(cnt, len(dijk_dist)))
+# print(bidijkTime, 'secs')
+# print('RESULT:')
+# print('Vertex count: ', graph.num_nodes)
+# print('Edge count: ', graph.num_edges)
+# print('Dijkstra time: ', dijk_time)
+# print('CH compute time: ', CHtime)
+# print('CH query time: ', bidijkTime)
+# cnt = 0
+# for i in range(0, len(dijk_dist)):
+#     if abs(dijk_dist[i][2] - bidijk_dist[i]) > PRECISION_TOLERANCE:
+#         cnt += 1
+#         print(dijk_dist[i][0], dijk_dist[i][1], dijk_dist[i][2], bidijk_dist[i])
+# print('{} / {}'.format(cnt, len(dijk_dist)))
